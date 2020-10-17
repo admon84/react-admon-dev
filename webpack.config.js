78afd31ef2812be537daac8f3f6fc880e4d48abe
admon84 <admonwest84@gmail.com>
2020-10-17 17:40:58 -0600

    New react project from scratch

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    plugins: [new webpack.ProgressPlugin()],
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: [path.resolve(__dirname, 'src')],
+                loader: 'babel-loader'
+            },
+            {
+                test: /.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader"}
+                ]
+            }
+        ]
+    }
+}

7de570d19f8b14351a92fdc4ebea10a4529bd029
admon84 <admonwest84@gmail.com>
2021-05-02 12:34:23 -0600

    Adding image to simplified homepage

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-import EmailIcon from '../images/email.svg';
-import GitHubIcon from '../images/github.svg';
-import DevIcon from '../images/dev.svg';
-
+import '../images/profile.png';
 import '../images/favicon-32.png';
 import '../images/favicon-16.png';
 
@@ -12,30 +9,20 @@ import '../styles/index.less';
 class App extends React.PureComponent {
     render() {
         return (
-            <main className="layout">
-                <div className="content">
-                    <div className="heading">
-                        <p>
-                            <span className="lime">~/</span>
-                            admon
-                            <span className="gray">.</span>
-                            dev
-                            <span className="lime">#</span>
-                        </p>
-                    </div>
-                    <ul className="link-list">
-                        <li>
-                            <a href="https://screeps.admon.dev">
-                                screeps tools
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/admon84">
-                                github
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+            <main className="content">
+                <p className="heading">
+                    <span className="mint">~/</span>
+                    admon
+                    <span className="gray">.</span>
+                    dev
+                    <span className="mint">#</span>
+                </p>
+                <a href="https://github.com/admon84" title="github/admon84">
+                    <img src="/profile.png" />
+                    <p className="author">
+                        admon84
+                    </p>
+                </a>
             </main>
         );
     }
